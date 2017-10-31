cd4a5a42c738de47137ba568d15fbdeb8d15dee8
Abhijith Asok <aasok@hsph.harvard.edu>
2017-10-31 12:39:32 -0400

    Added function and documentation

---
+++ R/linreg.R
@@ -0,0 +1,35 @@
+#' Linear regression
+#'
+#' Runs an OLS regression not unlike \code{\link{lm}}
+#'
+#' @param y response vector (1 x n)
+#' @param X covariate matrix (p x n) with no intercept
+#'
+#' @return A list with 4 elements: coefficients, vcov, sigma, df
+#'
+#' @examples
+#' data(mtcars)
+#' X <- as.matrix(mtcars[, c("cyl", "disp", "hp")])
+#' y <- mtcars[, "mpg"]
+#' linreg(y, X)
+#'
+#' @export
+#'
+linmodEst <- function(x, y) {
+  ## compute QR-decomposition of x
+  qx <- qr(x)
+  ## compute (x’x)^(-1) x’y
+  coef <- solve.qr(qx, y)
+  ## degrees of freedom and standard deviation of residuals
+  df <- nrow(x) - ncol(x)
+  sigma2 <- sum((y - x %*% coef) ^ 2) / df
+  ## compute sigma^2 * (x’x)^-1
+  vcov <- sigma2 * chol2inv(qx$qr)
+  colnames(vcov) <- rownames(vcov) <- colnames(x)
+  list(
+    coefficients = coef,
+    vcov = vcov,
+    sigma = sqrt(sigma2),
+    df = df
+  )
+}
